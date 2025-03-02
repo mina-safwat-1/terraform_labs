6d884bc2aaaba9cf1e06d2eba9d93a36978a25db
mina <mr.safwat55@gmail.com>
2025-03-02 13:19:17 +0200

    remove duplicate files

--- subnets.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_subnet" "subnets" {
-  for_each                = { for subnet in var.subnets : subnet.name => subnet }
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = each.value.cidr_block
-  map_public_ip_on_launch = each.value.type == "public" ? true : false
-  tags                    = { "Name" = each.value.name }
-}
